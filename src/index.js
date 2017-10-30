e783e5cde9ae78c53fbec77ea00a93fe6d54e393
Kuu Miyazaki <miyazaqui@gmail.com>
2017-10-31 01:21:31 +0900

    Update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import ElementClass from './skin';
 
+if (!customElements) {
+  throw new Error('Custom Elements not supported');
+}
+
 const name = 'skin-plugin-default';
 
 if (customElements.get(name)) {
   console.log(`Custom element "${name}" already defined`);
 } else {
-  window.customElements.define(name, ElementClass);
+  customElements.define(name, ElementClass);
 }
--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -1,54 +1,47 @@
 import '../../src/index';
 import '../../core/src/index';
 
-describe('player', () => {
-  it('should initialize/deinitialize skin (JS)', () => {
-    const player = document.createElement('video-player');
-    const skin = document.createElement('skin-plugin-default');
-    player.appendChild(skin);
-    expect(player.plugins.skin.size).toBe(0);
-    document.body.appendChild(player);
-    expect(player.plugins.skin.size).toBe(1);
-    player.removeChild(skin);
-    expect(player.plugins.skin.size).toBe(0);
-    document.body.removeChild(player);
-    expect(player.plugins.skin.size).toBe(0);
+describe('skin-plugin-default', () => {
+  describe('JS interface', () => {
+    beforeEach(() => {
+      const player = document.createElement('progressive-video-player');
+      const skin = document.createElement('skin-plugin-default');
+      player.appendChild(skin);
+      document.body.appendChild(player);
+    });
+
+    afterEach(() => {
+      const player = document.querySelector('progressive-video-player');
+      document.body.removeChild(player);
+    });
+
+    it('should initialize/deinitialize skin', () => {
+      const player = document.querySelector('progressive-video-player');
+      const skin = document.querySelector('skin-plugin-default');
+      expect(player.plugins.skin.size).toBe(1);
+      player.removeChild(skin);
+      expect(player.plugins.skin.size).toBe(0);
+    });
   });
 
-  it('should initialize/deinitialize skin (HTML)', () => {
-    const htmlTextPlayer = '<video-player></video-player>';
-    const htmlTextSkin = '<skin-plugin-default></skin-plugin-default>';
-    document.body.innerHTML = htmlTextPlayer;
-    const player = document.querySelector('video-player');
-    expect(player.plugins.skin.size).toBe(0);
-    player.innerHTML = htmlTextSkin;
-    expect(player.plugins.skin.size).toBe(1);
-    player.innerHTML = '';
-    expect(player.plugins.skin.size).toBe(0);
-    document.body.innerHTML = '';
-    expect(player.plugins.skin.size).toBe(0);
-  });
+  describe('HTML interface', () => {
+    beforeEach(() => {
+      document.body.innerHTML = `
+        <progressive-video-player src="http://xxx.com">
+          <skin-plugin-default></skin-plugin-default>
+        </progressive-video-player>
+      `;
+    });
 
-  it('should initialize/deinitialize skin 2 (JS)', () => {
-    const player = document.createElement('video-player');
-    const skin = document.createElement('skin-plugin-default');
-    player.appendChild(skin);
-    expect(player.plugins.skin.size).toBe(0);
-    document.body.appendChild(player);
-    expect(player.plugins.skin.size).toBe(1);
-    document.body.removeChild(player);
-    expect(player.plugins.skin.size).toBe(0);
-  });
+    afterEach(() => {
+      document.body.innerHTML = '';
+    });
 
-  it('should initialize/deinitialize skin 2 (HTML)', () => {
-    const htmlTextPlayer = '<video-player></video-player>';
-    const htmlTextSkin = '<skin-plugin-default></skin-plugin-default>';
-    document.body.innerHTML = htmlTextPlayer;
-    const player = document.querySelector('video-player');
-    expect(player.plugins.skin.size).toBe(0);
-    player.innerHTML = htmlTextSkin;
-    expect(player.plugins.skin.size).toBe(1);
-    document.body.innerHTML = '';
-    expect(player.plugins.skin.size).toBe(0);
+    it('should initialize/deinitialize skin', () => {
+      const player = document.querySelector('progressive-video-player');
+      expect(player.plugins.skin.size).toBe(1);
+      player.innerHTML = '';
+      expect(player.plugins.skin.size).toBe(0);
+    });
   });
 });
